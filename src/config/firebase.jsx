7a335ced63ded30c82ec91fb6bf98b3954974830
alex7842 <alex1958229@gmail.com>
2025-01-25 12:44:00 +0530

    djjd

--- src/config/firebase.jsx
+++ src/config/firebase.jsx
@@ -7,23 +7,23 @@ import { getMessaging } from 'firebase/messaging';
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCCaofRnxuQW5sO9v5ROVbOWQokxX7fpDA",
-  authDomain: "chatapp-81c34.firebaseapp.com",
-  projectId: "chatapp-81c34",
-  storageBucket: "chatapp-81c34.appspot.com",
-  messagingSenderId: "496298935969",
-  appId: "1:496298935969:web:d17d72db8d8af8e20f52e0"
-};
 // const firebaseConfig = {
-//   apiKey: "AIzaSyBbThrGm0hd6vvYxVTVmU85MwQug_6RNpQ",
-//   authDomain: "chatapp-demo-e7682.firebaseapp.com",
-//   projectId: "chatapp-demo-e7682",
-//   storageBucket: "chatapp-demo-e7682.firebasestorage.app",
-//   messagingSenderId: "568544912762",
-//   appId: "1:568544912762:web:d0d7c7f75bf94f447422c3",
-//   measurementId: "G-8ZZMYEPMBG"
+//   apiKey: "AIzaSyCCaofRnxuQW5sO9v5ROVbOWQokxX7fpDA",
+//   authDomain: "chatapp-81c34.firebaseapp.com",
+//   projectId: "chatapp-81c34",
+//   storageBucket: "chatapp-81c34.appspot.com",
+//   messagingSenderId: "496298935969",
+//   appId: "1:496298935969:web:d17d72db8d8af8e20f52e0"
 // };
+const firebaseConfig = {
+  apiKey: "AIzaSyBbThrGm0hd6vvYxVTVmU85MwQug_6RNpQ",
+  authDomain: "chatapp-demo-e7682.firebaseapp.com",
+  projectId: "chatapp-demo-e7682",
+  storageBucket: "chatapp-demo-e7682.firebasestorage.app",
+  messagingSenderId: "568544912762",
+  appId: "1:568544912762:web:d0d7c7f75bf94f447422c3",
+  measurementId: "G-8ZZMYEPMBG"
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
